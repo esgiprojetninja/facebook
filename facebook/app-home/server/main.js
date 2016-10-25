1b92d571565cb1090e4e1fce333cde1132906e51
Meksavanh <ventialaw@hotmail.fr>
2016-10-25 14:05:48 +0200

    Clé non divulgué sur le repo mdr

--- facebook/app-home/server/main.js
+++ facebook/app-home/server/main.js
@@ -1,14 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-    ServiceConfiguration.configurations.remove({
-        service: 'facebook'
-    });
 
-    ServiceConfiguration.configurations.insert({
-        service: 'facebook',
-        appId: '1200139990052440',
-        secret: '7ed0f55fd08612a805b851fa6fbde893'
-    });
 });
