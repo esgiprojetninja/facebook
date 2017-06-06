87cbcc602018aeee1fa42e61c0cf8e5cf3ad1cd4
tmeksava <teddy.meksavanh@arkena.com>
2017-06-07 01:11:49 +0200

    tsv

--- facebook/app-home/client/main.js
+++ facebook/app-home/client/main.js
@@ -2,194 +2,422 @@ import './main.html';
 
 var d3 = require('d3');
 
-// Db on mongodb://127.0.0.1:3001/meteor
-// var db = new MongoInternals.RemoteCollectionDriver('mongodb://127.0.0.1:3001/meteor');
-
-// returns instance of Foo2
 Template.home.onCreated(function helloOnCreated() {
-  this.subscribe = new ReactiveVar(false);
+    this.subscribe = new ReactiveVar(false);
 });
 
-Template.home.rendered = function(){
-  "use strict";
-  connectFront.init();
-  cookieBar.init();
-  // d3Instance.init();
+// La fonction rendered contient le JS qui va s'exécuter lorsque l'affichage sera crée
+Template.home.rendered = function() {
+    "use strict";
+    connectFront.init();
+    cookieBar.init();
 
-  //Question 1 : Utilisateur 11
+    // Question 1 :
 
-  // var numberOfRelations = db.open('relations').find().count();
-  // var userRelations = db.open('relations').find().fetch();
+    Meteor.call('getRelationsUser', 11, function(error, response) {
+        if (error) {
+            alert('getRelationsUser is empty');
+        } else {
+            let relationsDate = [];
+            let line1 = [];
 
-  Meteor.call('getRelations', function(error, result){
-    if(error){
-      alert('cant get relations db');
-    }else{
-      console.log('result', result);
-    }
-  });
+            // Si la requête marche on implémente le graphique
+            for (var i = 0; i < response.length; i++) {
+                relationsDate.push(response[i].date);
+            }
 
-  var line1=[['2008-08-12 4:00PM',4], ['2008-09-12 4:00PM',6.5], ['2008-10-12 4:00PM',5.7], ['2008-11-12 4:00PM',9], ['2008-12-12 4:00PM',8.2]];
-  var plot1 = $.jqplot('chart1', [line1], {
-    title:'Default Date Axis',
-    axes:{
-        xaxis:{
-            renderer:$.jqplot.DateAxisRenderer
+            let numberOfOccurence = compressArray(relationsDate);
+
+            for (var j = 0; j < numberOfOccurence.length; j++) {
+                line1.push([numberOfOccurence[j].value + ' 4:00PM', numberOfOccurence[j].count]);
+            }
+
+            let plot1 = $.jqplot('chart1', [line1], {
+                title: "Evolution du nombre d’amis au fil du mois",
+                axes: {
+                    xaxis: {
+                        renderer: $.jqplot.DateAxisRenderer
+                    }
+                },
+                series: [{
+                    lineWidth: 4,
+                    markerOptions: {
+                        style: 'square'
+                    }
+                }]
+            });
         }
-    },
-    series:[{lineWidth:4, markerOptions:{style:'square'}}]
-  });
+    });
+
+    // Question 2 :
+
+    Meteor.call('getNotationsUser', 11, function(error, response) {
+        if (error) {
+            alert('getNotationsUser is empty');
+        } else {
+            let relationsDate = [];
+            let line1 = [];
+
+            // Si la requête marche on implémente le graphique
+            for (var i = 0; i < response.length; i++) {
+                relationsDate.push(response[i].date);
+            }
+
+            let numberOfOccurence = compressArray(relationsDate);
+
+            for (var j = 0; j < numberOfOccurence.length; j++) {
+                line1.push([numberOfOccurence[j].value + ' 4:00PM', numberOfOccurence[j].count]);
+            }
+
+            let plot1 = $.jqplot('chart2', [line1], {
+                title: "Evolution de la popularité au fil du mois",
+                axes: {
+                    xaxis: {
+                        renderer: $.jqplot.DateAxisRenderer
+                    }
+                },
+                series: [{
+                    lineWidth: 4,
+                    markerOptions: {
+                        style: 'square'
+                    }
+                }]
+            });
+        }
+    });
+
+    // Question 3:
+
+    Meteor.call('getMessagesUser', 11, function(error, response) {
+        if (error) {
+            alert('getMessagesUser is empty');
+        } else {
+
+            d3Chart3.init(response);
+        }
+    });
+
+    // d3Chart3.init();
+
+
 };
 
-Template.datavizualisation.rendered = function() {
-  d3Instance.init();
+Template.datavizualisation.rendered = function()  {
+    d3Instance.init();
 };
 
-Template.home.events({
-  'click #ninja-btn-login' : function(){
-    console.log("Login.");
-  },
-  'click #connect-bottom-subscribe'(event, template){
-    event.preventDefault();
+// Template.home.events({
+//   'click #ninja-btn-login' : function(){
+//     console.log("Login.");
+//   },
+//   'click #connect-bottom-subscribe'(event, template){
+//     event.preventDefault();
+//
+//     var subscribe = template.subscribe.get();
+//     template.subscribe.set(!subscribe);
+//   },
+//   'click #facebook-login': function(event) {
+//     console.log("Facebook login.");
+//
+//     Meteor.loginWithFacebook({}, function(err){
+//       if(err){
+//         throw new Meteor.Error("Facebook login failed");
+//         jQuery('#myModal').modal("show");
+//       }else{
+//         jQuery('#myModal').modal("show");
+//       }
+//     });
+//   },
+//   'click #facebook-logout': function(event) {
+//     console.log("Facebook logout.");
+//
+//     Meteor.logout(function(err){
+//       if (err) {
+//         throw new Meteor.Error("Facebook logout failed");
+//         jQuery('#myModal').modal("hide");
+//       }else{
+//         jQuery('#myModal').modal("hide");
+//       }
+//     });
+//   }
+// });
 
-    var subscribe = template.subscribe.get();
-    template.subscribe.set(!subscribe);
-  },
-  'click #facebook-login': function(event) {
-    console.log("Facebook login.");
-
-    Meteor.loginWithFacebook({}, function(err){
-      if(err){
-        throw new Meteor.Error("Facebook login failed");
-        jQuery('#myModal').modal("show");
-      }else{
-        jQuery('#myModal').modal("show");
-      }
-    });
-  },
-  'click #facebook-logout': function(event) {
-    console.log("Facebook logout.");
-
-    Meteor.logout(function(err){
-      if (err) {
-        throw new Meteor.Error("Facebook logout failed");
-        jQuery('#myModal').modal("hide");
-      }else{
-        jQuery('#myModal').modal("hide");
-      }
-    });
-  }
-});
-
-Template.home.helpers({
-  isSubscribe : function() {
-    return Template.instance().subscribe.get();
-  },
-  userImage: function(){
-    if(Meteor.user().services.facebook){
-      return 'https://graph.facebook.com/' + Meteor.user().services.facebook.id + '/picture/?type=large';
-    }else{
-      return 'http://placehold.it/40×40';
-    }
-  }
-});
+// Template.home.helpers({
+//   isSubscribe : function() {
+//     return Template.instance().subscribe.get();
+//   },
+//   userImage: function(){
+//     if(Meteor.user().services.facebook){
+//       return 'https://graph.facebook.com/' + Meteor.user().services.facebook.id + '/picture/?type=large';
+//     }else{
+//       return 'http://placehold.it/40×40';
+//     }
+//   }
+// });
 
 /** Canvas animation **/
 var connectFront = {
-  init : function(){
-    connectFront.title();
-  },
-  title: function () {
+    init: function() {
+        connectFront.title();
+    },
+    title: function() {
 
-    setTimeout(function () {
-      let title = jQuery(".connect .svg-canvas path");
+        setTimeout(function() {
+            let title = jQuery(".connect .svg-canvas path");
 
-      title.css('fill-opacity', '1');
-    }, 3000);
-  }
+            title.css('fill-opacity', '1');
+        }, 3000);
+    }
 };
 
+var d3Chart3 = {
+    init: function(data) {
+
+        var svg = d3.select("svg"),
+            margin = {
+                top: 20,
+                right: 20,
+                bottom: 30,
+                left: 40
+            },
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom;
+
+        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+            y = d3.scaleLinear().rangeRound([height, 0]);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(data.map(function(d) {
+          // if (error) throw error;
+
+            x.domain(data.map(function(d) {
+                return d.destinataire;
+            }));
+
+            d.frequency = 1;
+
+            console.log(d);
+
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y).ticks(10, "%"))
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .attr("text-anchor", "end")
+                .text("Frequency");
+
+            g.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) {
+                    return x(d.destinataire);
+                })
+                .attr("y", function(d) {
+                    return y(d.frequency);
+                })
+                .attr("width", x.bandwidth())
+                .attr("height", function(d) {
+                    return height - y(d.frequency);
+                });
+        }));
+
+        // d3.tsv("data.tsv", function(d) {
+        //     d.frequency = +d.frequency;
+        //     return d;
+        // }, function(error, data) {
+        //     if (error) throw error;
+        //
+        //     x.domain(data.map(function(d) {
+        //         return d.letter;
+        //     }));
+        //     y.domain([0, d3.max(data, function(d) {
+        //         return d.frequency;
+        //     })]);
+        //
+        //     g.append("g")
+        //         .attr("class", "axis axis--x")
+        //         .attr("transform", "translate(0," + height + ")")
+        //         .call(d3.axisBottom(x));
+        //
+        //     g.append("g")
+        //         .attr("class", "axis axis--y")
+        //         .call(d3.axisLeft(y).ticks(10, "%"))
+        //         .append("text")
+        //         .attr("transform", "rotate(-90)")
+        //         .attr("y", 6)
+        //         .attr("dy", "0.71em")
+        //         .attr("text-anchor", "end")
+        //         .text("Frequency");
+        //
+        //     g.selectAll(".bar")
+        //         .data(data)
+        //         .enter().append("rect")
+        //         .attr("class", "bar")
+        //         .attr("x", function(d) {
+        //             return x(d.letter);
+        //         })
+        //         .attr("y", function(d) {
+        //             return y(d.frequency);
+        //         })
+        //         .attr("width", x.bandwidth())
+        //         .attr("height", function(d) {
+        //             return height - y(d.frequency);
+        //         });
+        // });
+    }
+}
+
 var d3Instance = {
-  init : function() {
+    init: function() {
 
-    var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+        var svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height");
 
-    var format = d3.format(",d");
+        var format = d3.format(",d");
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20c);
+        var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
-    var pack = d3.pack()
-    .size([width, height])
-    .padding(1.5);
+        var pack = d3.pack()
+            .size([width, height])
+            .padding(1.5);
 
-    d3.csv("flare.csv", function(d) {
-      d.value = +d.value;
-      if (d.value) return d;
-    }, function(error, classes) {
-      if (error) throw error;
+        d3.csv("flare.csv", function(d) {
+            d.value = +d.value;
+            if (d.value) return d;
+        }, function(error, classes) {
+            if (error) throw error;
 
-      var root = d3.hierarchy({children: classes})
-      .sum(function(d) { return d.value; })
-      .each(function(d) {
-        if (id = d.data.id) {
-          var id, i = id.lastIndexOf(".");
-          d.id = id;
-          d.package = id.slice(0, i);
-          d.class = id.slice(i + 1);
-        }
-      });
+            var root = d3.hierarchy({
+                    children: classes
+                })
+                .sum(function(d) {
+                    return d.value;
+                })
+                .each(function(d) {
+                    if (id = d.data.id) {
+                        var id, i = id.lastIndexOf(".");
+                        d.id = id;
+                        d.package = id.slice(0, i);
+                        d.class = id.slice(i + 1);
+                    }
+                });
 
-      var node = svg.selectAll(".node")
-      .data(pack(root).leaves())
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            var node = svg.selectAll(".node")
+                .data(pack(root).leaves())
+                .enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function(d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
 
-      node.append("circle")
-      .attr("id", function(d) { return d.id; })
-      .attr("r", function(d) { return d.r; })
-      .style("fill", function(d) { return color(d.package); });
+            node.append("circle")
+                .attr("id", function(d) {
+                    return d.id;
+                })
+                .attr("r", function(d) {
+                    return d.r;
+                })
+                .style("fill", function(d) {
+                    return color(d.package);
+                });
 
-      node.append("clipPath")
-      .attr("id", function(d) { return "clip-" + d.id; })
-      .append("use")
-      .attr("xlink:href", function(d) { return "#" + d.id; });
+            node.append("clipPath")
+                .attr("id", function(d) {
+                    return "clip-" + d.id;
+                })
+                .append("use")
+                .attr("xlink:href", function(d) {
+                    return "#" + d.id;
+                });
 
-      node.append("text")
-      .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-      .selectAll("tspan")
-      .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-      .enter().append("tspan")
-      .attr("x", 0)
-      .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-      .text(function(d) { return d; });
+            node.append("text")
+                .attr("clip-path", function(d) {
+                    return "url(#clip-" + d.id + ")";
+                })
+                .selectAll("tspan")
+                .data(function(d) {
+                    return d.class.split(/(?=[A-Z][^A-Z])/g);
+                })
+                .enter().append("tspan")
+                .attr("x", 0)
+                .attr("y", function(d, i, nodes) {
+                    return 13 + (i - nodes.length / 2 - 0.5) * 10;
+                })
+                .text(function(d) {
+                    return d;
+                });
 
-      node.append("title")
-      .text(function(d) { return d.id + "\n" + format(d.value); });
-    });
+            node.append("title")
+                .text(function(d) {
+                    return d.id + "\n" + format(d.value);
+                });
+        });
 
-  }
+    }
 };
 
 
 /** Cookie bar **/
 var cookieBar = {
-  init : function(){
-    cookieBar.setCookieBar();
+    init: function() {
+        cookieBar.setCookieBar();
 
-    cookieBar.onClick();
-  },
-  setCookieBar : function(){
-    this._cookieBar = jQuery(".cookie-bar");
-  },
-  getCookieBar : function(){
-    return this._cookieBar;
-  },
-  onClick : function(){
-    cookieBar.getCookieBar().on("click", function(e){
-      jQuery(this).css({"transform" : "translateY(-100%)", "transition" : "all .3s"})
-    });
-  }
+        cookieBar.onClick();
+    },
+    setCookieBar: function() {
+        this._cookieBar = jQuery(".cookie-bar");
+    },
+    getCookieBar: function() {
+        return this._cookieBar;
+    },
+    onClick: function() {
+        cookieBar.getCookieBar().on("click", function(e) {
+            jQuery(this).css({
+                "transform": "translateY(-100%)",
+                "transition": "all .3s"
+            })
+        });
+    }
+};
+
+function compressArray(original) {
+
+    var compressed = [];
+    // make a copy of the input array
+    var copy = original.slice(0);
+
+    // first loop goes over every element
+    for (var i = 0; i < original.length; i++) {
+
+        var myCount = 0;
+        // loop over every element in the copy and see if it's the same
+        for (var w = 0; w < copy.length; w++) {
+            if (original[i] == copy[w]) {
+                // increase amount of times duplicate is found
+                myCount++;
+                // sets item to undefined
+                delete copy[w];
+            }
+        }
+
+        if (myCount > 0) {
+            var a = new Object();
+            a.value = original[i];
+            a.count = myCount;
+            compressed.push(a);
+        }
+    }
+
+    return compressed;
 };
--- facebook/app-home/server/main.js
+++ facebook/app-home/server/main.js
@@ -2,16 +2,77 @@ import { Meteor } from 'meteor/meteor';
 import { MongoInternals } from 'meteor/mongo';
 import { Mongo } from 'meteor/mongo';
 
+// La variable contenant la bdd Mongo sur le port 3001
+const db = new MongoInternals.RemoteCollectionDriver('mongodb://127.0.0.1:3001/meteor');
+
+// Au démarage de Meteor on va exécuter des fonctions pour récupérer les données dans la bdd
 Meteor.startup(() => {
+
+  // Ces méthodes sont créer coté serveur afin de sécuriser les données
   Meteor.methods({
+
+    // { tags: { $all: [ "ssl" , "security" ] } }
+
+    // Méthode Get de chaques collections
 	  getRelations: function () {
-      var db = new MongoInternals.RemoteCollectionDriver('mongodb://127.0.0.1:3001/meteor');
       var numberOfRelations = db.open('relations').find().count();
       var userRelations = db.open('relations').find().fetch();
 
-      console.log('userRelations', userRelations);
-
 	    return userRelations;
-	  }
+	  },
+    getRelationsUser: function(number) {
+      var numberOfRelations = db.open('relations').find().count();
+      var user = db.open('relations').find({user1: number}).fetch();
+
+      return user;
+    },
+    getNotations: function () {
+      var numberOfNotations = db.open('notations').find().count();
+      var userNotations = db.open('notations').find().fetch();
+
+      return userNotations;
+    },
+    getNotationsUser: function(number) {
+      var numberOfNotations = db.open('notations').find().count();
+      var user = db.open('notations').find({noteur: number}).fetch();
+
+      return user;
+    },
+    getMessages: function () {
+      var numberOfMessages = db.open('messages').find().count();
+      var userMessages = db.open('messages').find().fetch();
+
+      return userMessages;
+    },
+    getMessagesUser: function(number) {
+      var numberOfMessages = db.open('messages').find().count();
+      var user = db.open('messages').find({emetteur: number}).fetch();
+
+      return user;
+    },
+    getPhotos: function () {
+      var numberOfPhotos = db.open('photos').find().count();
+      var userPhotos = db.open('photos').find().fetch();
+
+      return userPhotos;
+    },
+    getStatus: function () {
+      var numberOfStatus = db.open('status').find().count();
+      var userStatus = db.open('status').find().fetch();
+
+      return userStatus;
+    },
+    getUtilisateurs: function () {
+      var numberOfUtilisateurs = db.open('utilisateurs').find().count();
+      var userUtilisateurs = db.open('utilisateurs').find().fetch();
+
+      return userUtilisateurs;
+    },
+    getUtilisateursById: function (id) {
+      var numberOfUtilisateurs = db.open('utilisateurs').find().count();
+      var user = db.open('utilisateurs').find({id: id}).fetch();
+
+      return user;
+    }
   });
 });
